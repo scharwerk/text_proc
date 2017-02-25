8b91e58d6fc7e9eeef609d1487f641f79314e770
Chmen <tony.potapov@gmail.com>
2017-02-25 13:19:42 +0200

    add first test for remove_line_breaks_method

--- spec/text_processing_spec.rb
+++ spec/text_processing_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe TextProcessing do
-
+  describe '.line_break_remove' do
+    it 'return text without line break' do
+      text = '...обставин і подання позичкового гро-
+              шового капіталу, як пізніше виявиться'
+      text_without_breacks = '...обставин і подання позичкового гро
+                              шового капіталу, як пізніше виявиться'
+      expect(TextProcessing::line_break_remove(text)).to eq text 
+    end  
+  end
 end
--- text_processing.rb
+++ text_processing.rb
@@ -1,3 +1,8 @@
 module TextProcessing
 
+  def line_break_remove(text)
+
+  end
+  module_function :line_break_remove
+
 end
