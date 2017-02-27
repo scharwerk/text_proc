0a3758305651755a8e420d1d74d42419300e1388
Chmen <tony.potapov@gmail.com>
2017-02-27 21:22:09 +0200

    add test for multiline text

--- spec/text_processing_spec.rb
+++ spec/text_processing_spec.rb
@@ -1,13 +1,35 @@
 require 'spec_helper'
 
 describe TextProcessing do
+
   describe '.line_break_remove' do
-    it 'return text without line break' do
-      text = '...обставин і подання позичкового гро-
-              шового капіталу, як пізніше виявиться'
-      text_without_breacks = '...обставин і подання позичкового гро
-                              шового капіталу, як пізніше виявиться'
-      expect(TextProcessing.line_break_remove(text)).to eq text 
-    end  
+    context 'with a single line' do
+      it 'return a text without line break' do
+        text = '...обставин і подання позичкового гро-'
+        text_without_breacks = '...обставин і подання позичкового гро'
+        expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks
+      end
+
+      it 'return text with hyphen and dash' do
+        text = 'кінець-кінцем — лише ...), як це буває при ку-'
+        text_without_breacks = 'кінець-кінцем — лише ...), як це буває при ку'
+      end
+    end
+
+    context 'with a multiline text' do
+      it 'concatenate word with line breack' do
+        text =
+          'капіталу ще немає мови) меншає, і тому капіталу, призначеного для позик,
+             стає порівняно багато. Але серед таких обставин і подання позичкового гро-
+             шового капіталу, як пізніше виявиться, позитивно зростає.'
+          text_without_breacks =
+          'капіталу ще немає мови) меншає, і тому капіталу, призначеного для позик,
+             стає порівняно багато. Але серед таких обставин і подання позичкового грошового
+             капіталу, як пізніше виявиться, позитивно зростає.'
+
+          expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks 
+          end
+    end
   end
+
 end
--- text_processing.rb
+++ text_processing.rb
@@ -1,7 +1,7 @@
 class TextProcessing
 
   def self.line_break_remove(text)
-    
+    text.gsub(/-$/, "")  
   end
-
+# try to delete in couple with a line end symbol
 end
