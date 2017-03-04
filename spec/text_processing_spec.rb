647b84b0b9b3689ec40be63b3a524604cda58e91
denys <denys.potapov@gmail.com>
2017-03-04 14:44:17 +0200

    add change_qoutes method with test

--- spec/text_processing_spec.rb
+++ spec/text_processing_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe TextProcessing do
 
-  describe '.line_break_remove' do
-    context 'with a single line' do
-      it 'return a text without line break' do
-        text = '...обставин і подання позичкового гро-'
-        text_without_breacks = '...обставин і подання позичкового гро'
-        expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks
-      end
+  # describe '.line_break_remove' do
+  #   context 'with a single line' do
+  #     it 'return a text without line break' do
+  #       text = '...обставин і подання позичкового гро-'
+  #       text_without_breacks = '...обставин і подання позичкового гро'
+  #       expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks
+  #     end
 
-      it 'return text with hyphen and dash' do
-        text = 'кінець-кінцем — лише ...), як це буває при ку-'
-        text_without_breacks = 'кінець-кінцем — лише ...), як це буває при ку'
-      end
-    end
+  #     it 'return text with hyphen and dash' do
+  #       text = 'кінець-кінцем — лише ...), як це буває при ку-'
+  #       text_without_breacks = 'кінець-кінцем — лише ...), як це буває при ку'
+  #     end
+  #   end
 
     # context 'with a multiline text' do
     #   it 'concatenate word with line breack' do
@@ -27,36 +27,50 @@ describe TextProcessing do
     #          стає порівняно багато. Але серед таких обставин і подання позичкового грошового
     #          капіталу, як пізніше виявиться, позитивно зростає.'
 
-    #       expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks 
+    #       expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks
     #       end
     # end
-  end
+  # end
 
-  describe '.ends_with_hyphen?' do
-    it 'return true if ends with hyphen' do
-      text = 'багато. Але серед таких обставин і подання позичкового гро-'
-      expect(TextProcessing.ends_with_hyphen?(text)).to be true
-    end
-    it 'return false if ends with hyphen' do
-      text = 'і тому капіталу, призначеного для позик,'
-      expect(TextProcessing.ends_with_hyphen?(text)).to be false
-    end
-  end
+  # describe '.ends_with_hyphen?' do
+  #   it 'return true if ends with hyphen' do
+  #     text = 'багато. Але серед таких обставин і подання позичкового гро-'
+  #     expect(TextProcessing.ends_with_hyphen?(text)).to be true
+  #   end
+  #   it 'return false if ends with hyphen' do
+  #     text = 'і тому капіталу, призначеного для позик,'
+  #     expect(TextProcessing.ends_with_hyphen?(text)).to be false
+  #   end
+  # end
 
-  describe '.remove_last_word' do
-    it 'return string without last word' do
-      text = 'багато. Але серед таких обставин і подання позичкового гро-'
-      cuted_text = 'багато. Але серед таких обставин і подання позичкового'
-      expect(TextProcessing.remove_last_word(text)).to eq cuted_text
-    end
-  end
+  # describe '.remove_last_word' do
+  #   it 'return string without last word' do
+  #     text = 'багато. Але серед таких обставин і подання позичкового гро-'
+  #     cuted_text = 'багато. Але серед таких обставин і подання позичкового'
+  #     expect(TextProcessing.remove_last_word(text)).to eq cuted_text
+  #   end
+  # end
 
-  
-  describe '.remove_last_word' do
-    it 'return string without last word' do
-      text = 'багато. Але серед таких обставин і подання позичкового гро-'
-      cuted_text = 'Але серед таких обставин і подання позичкового гро-'
-      expect(TextProcessing.remove_first_word(text)).to eq cuted_text
+
+  # describe '.remove_last_word' do
+  #   it 'return string without last word' do
+  #     text = 'багато. Але серед таких обставин і подання позичкового гро-'
+  #     cuted_text = 'Але серед таких обставин і подання позичкового гро-'
+  #     expect(TextProcessing.remove_first_word(text)).to eq cuted_text
+  #   end
+  # end
+
+  describe '.change_quotes' do
+    it 'return text with «...» instead „...“' do
+      text1 =  ' с., р. 40). „Навпаки,
+        єдино правильний метод той, щоб на доходи кожного року покладати
+        зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
+        одно, чи витрачено дану суму, чи ні“ (Captain '
+      text2 =  ' с., р. 40). «Навпаки,
+        єдино правильний метод той, щоб на доходи кожного року покладати
+        зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
+        одно, чи витрачено дану суму, чи ні» (Captain '
+      expect(TextProcessing.change_quotes(text1)).to eq text2
     end
   end
 
--- text_processing.rb
+++ text_processing.rb
@@ -1,19 +1,24 @@
 class TextProcessing
 
-  def self.line_break_remove(text)
-    text.gsub(/-$/, "")  
-  end
+  # def self.line_break_remove(text)
+  #   text.gsub(/-$/, "")  
+  # end
 
-  def self.ends_with_hyphen?(line)
-    line.end_with?('-')
-  end
+  # def self.ends_with_hyphen?(line)
+  #   line.end_with?('-')
+  # end
 
-  def self.remove_last_word(line)
-    line[0...line.rindex(' ')]
-  end
+  # def self.remove_last_word(line)
+  #   line[0...line.rindex(' ')]
+  # end
 
-  def self.remove_first_word(line)
-    line[1...(line.rindex(' ')+1)]
+  # def self.remove_first_word(line)
+  #   line[1...(line.rindex(' ')+1)]
+  # end
+  
+  def self.change_quotes(text)
+    text.gsub(/[„“]/, '„' => '«', '“' => '»')
+    #'hello'.gsub(/[eo]/, 'e' => 3, 'o' => '*') 
   end
 
 # try to delete in couple with a line end symbol
