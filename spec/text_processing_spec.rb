33a959d9270ca2d93f39ab3717810dda4ef94000
Chmen <tony.potapov@gmail.com>
2017-03-12 23:09:09 +0200

    refactor with rubocop

--- spec/text_processing_spec.rb
+++ spec/text_processing_spec.rb
@@ -1,65 +1,6 @@
 require 'spec_helper'
 
 describe TextProcessing do
-
-  # describe '.line_break_remove' do
-  #   context 'with a single line' do
-  #     it 'return a text without line break' do
-  #       text = '...обставин і подання позичкового гро-'
-  #       text_without_breacks = '...обставин і подання позичкового гро'
-  #       expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks
-  #     end
-
-  #     it 'return text with hyphen and dash' do
-  #       text = 'кінець-кінцем — лише ...), як це буває при ку-'
-  #       text_without_breacks = 'кінець-кінцем — лише ...), як це буває при ку'
-  #     end
-  #   end
-
-    # context 'with a multiline text' do
-    #   it 'concatenate word with line breack' do
-    #     text =
-    #       'капіталу ще немає мови) меншає, і тому капіталу, призначеного для позик,
-    #          стає порівняно багато. Але серед таких обставин і подання позичкового гро-
-    #          шового капіталу, як пізніше виявиться, позитивно зростає.'
-    #       text_without_breacks =
-    #       'капіталу ще немає мови) меншає, і тому капіталу, призначеного для позик,
-    #          стає порівняно багато. Але серед таких обставин і подання позичкового грошового
-    #          капіталу, як пізніше виявиться, позитивно зростає.'
-
-    #       expect(TextProcessing.line_break_remove(text)).to eq text_without_breacks
-    #       end
-    # end
-  # end
-
-  # describe '.ends_with_hyphen?' do
-  #   it 'return true if ends with hyphen' do
-  #     text = 'багато. Але серед таких обставин і подання позичкового гро-'
-  #     expect(TextProcessing.ends_with_hyphen?(text)).to be true
-  #   end
-  #   it 'return false if ends with hyphen' do
-  #     text = 'і тому капіталу, призначеного для позик,'
-  #     expect(TextProcessing.ends_with_hyphen?(text)).to be false
-  #   end
-  # end
-
-  # describe '.remove_last_word' do
-  #   it 'return string without last word' do
-  #     text = 'багато. Але серед таких обставин і подання позичкового гро-'
-  #     cuted_text = 'багато. Але серед таких обставин і подання позичкового'
-  #     expect(TextProcessing.remove_last_word(text)).to eq cuted_text
-  #   end
-  # end
-
-
-  # describe '.remove_last_word' do
-  #   it 'return string without last word' do
-  #     text = 'багато. Але серед таких обставин і подання позичкового гро-'
-  #     cuted_text = 'Але серед таких обставин і подання позичкового гро-'
-  #     expect(TextProcessing.remove_first_word(text)).to eq cuted_text
-  #   end
-  # end
-
   describe '.change_quotes' do
     it 'return text with «...» instead „...“' do
       text1 =  ' с., р. 40). „Навпаки,
@@ -76,54 +17,54 @@ describe TextProcessing do
 
   describe '.remove_trailing_whitespace' do
     it 'remove space in an end of a line' do
-       text1 =  'Навпаки,
+      text1 =  'Навпаки,
 єдино правильний метод той, щоб на доходи кожного року покладати 
 зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
 одно, чи витрачено дану суму, чи ні“ (Captain'
-       text2 =  'Навпаки,
+      text2 =  'Навпаки,
 єдино правильний метод той, щоб на доходи кожного року покладати
 зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
 одно, чи витрачено дану суму, чи ні“ (Captain'
-       expect(TextProcessing.remove_trailing_whitespace(text1)).to eq text2
+      expect(TextProcessing.remove_trailing_whitespace(text1)).to eq text2
     end
     it 'remove space in a begining of a line' do
-       text1 =  'Навпаки,
+      text1 =  'Навпаки,
  єдино правильний метод той, щоб на доходи кожного року покладати
 зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
 одно, чи витрачено дану суму, чи ні“ (Captain'
-       text2 =  'Навпаки,
+      text2 =  'Навпаки,
 єдино правильний метод той, щоб на доходи кожного року покладати
 зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
 одно, чи витрачено дану суму, чи ні“ (Captain'
-       expect(TextProcessing.remove_trailing_whitespace(text1)).to eq text2
+      expect(TextProcessing.remove_trailing_whitespace(text1)).to eq text2
     end
     it 'remove multiple spaces' do
-       text1 =  'Навпаки,
+      text1 =  'Навпаки,
 єдино  правильний метод той, щоб на доходи кожного року покладати
 зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
 одно, чи витрачено дану суму, чи ні“ (Captain'
-       text2 =  'Навпаки,
+      text2 =  'Навпаки,
 єдино правильний метод той, щоб на доходи кожного року покладати
 зневартнення, неминуче зв’язане з тим, щоб заслужити ці доходи, все
 одно, чи витрачено дану суму, чи ні“ (Captain'
-       expect(TextProcessing.remove_trailing_whitespace(text1)).to eq text2
+      expect(TextProcessing.remove_trailing_whitespace(text1)).to eq text2
     end
   end
 
   describe '.lowercase_headings' do
-    it 'replace all laters to lowercase except first' do 
+    it 'replace all laters to lowercase except first' do
       text1 = 'КРУГОБІГ ТОВАРОВОГО КАПІТАЛУ'
       text2 = 'Кругобіг товарового капіталу'
       expect(TextProcessing.lowercase_headings(text1)).to eq text2
     end
-  end 
+  end
 
-  describe '.uppercase_line?' do 
+  describe '.uppercase_line?' do
     context 'a line with all uppercase laters' do
-        it 'return true' do
-          line = 'КРУГОБІГ ТОВАРОВОГО КАПІТАЛУ'
-          expect(TextProcessing.uppercase_line?(line)).to be true
-        end
+      it 'return true' do
+        line = 'КРУГОБІГ ТОВАРОВОГО КАПІТАЛУ'
+        expect(TextProcessing.uppercase_line?(line)).to be true
+      end
     end
     context 'a ordinary line' do
       it 'return false' do
@@ -132,9 +73,7 @@ describe TextProcessing do
       end
     end
     context 'with a only formula on line' do
-      line = 'Т— Г —Т'
-    end  
+      # line = 'Т— Г —Т' # /Р
     end
   end
-
 end
--- text_processing.rb
+++ text_processing.rb
@@ -1,7 +1,7 @@
+# This is a class for automatic text upgrade
 class TextProcessing
-
   # def self.line_break_remove(text)
-  #   text.gsub(/-$/, "")  
+  #   text.gsub(/-$/, "")
   # end
 
   # def self.ends_with_hyphen?(line)
@@ -15,7 +15,7 @@ class TextProcessing
   # def self.remove_first_word(line)
   #   line[1...(line.rindex(' ')+1)]
   # end
-  
+
   def self.change_quotes(text)
     text.gsub(/[„“]/, '„' => '«', '“' => '»')
   end
@@ -26,14 +26,14 @@ class TextProcessing
 
   def self.lowercase_headings(text)
     text.capitalize
-    #good news it will work for ruby 2.4.0
-    #case methods works only with English
-    #case conversion is effective only in ASCII region.
+    # good news it will work for ruby 2.4.0
+    # case methods works only with English
+    # case conversion is effective only in ASCII region.
   end
 
   def self.uppercase_line?(line)
-    line.match?(/\p{Upper}/) and !line.match?(/\p{Lower}/)
-    #if we met any Lowercase char
+    line.match?(/\p{Upper}/) && !line.match?(/\p{Lower}/)
+    # if we met any Lowercase char
   end
-# try to delete in couple with a line end symbol
+  # try to delete in couple with a line end symbol
 end
